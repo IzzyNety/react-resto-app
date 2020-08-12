6f7e6c477f147bbbe7ac60ea5c2b0b221c53e04b
IzzyNety <izzy.nety@gmail.com>
2020-08-12 14:29:42 +0500

    События + рефакторинг кода

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import {MainPage, CartPage, ItemPage} from '../pages';
 import AppHeader from '../app-header';
 
 import Background from './food-bg.jpg';
@@ -8,11 +8,11 @@ import {Route, Switch} from 'react-router-dom';
 const App = () => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
+            <AppHeader/>
             <Switch>
                 <Route path='/' exact component={MainPage}/>
                 <Route path='/cart' exact component={CartPage}/>
-                <Route exact component={MainPage}/>
+                <Route path='/:id' component={ItemPage}/>
             </Switch>
         </div>
     )
